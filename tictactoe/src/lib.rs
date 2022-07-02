ac3afdafe5010c5b58cd8ffe3bb74e21f7d1d671
janosimas <janosimas@gmail.com>
2022-07-02 21:32:03 +0200

    WIP: continue tictactoe

--- tictactoe/src/lib.rs
+++ tictactoe/src/lib.rs
@@ -3,11 +3,13 @@
 
 use std::fmt::Display;
 
+use async_trait::async_trait;
 use color_eyre::eyre::bail;
 use color_eyre::Result;
 use serde::{Deserialize, Serialize};
+use tabua_utils::Board2D;
 
-#[derive(Serialize, Deserialize, Clone, Copy, Debug, PartialEq)]
+#[derive(Serialize, Deserialize, Clone, Copy, Debug, PartialEq, Eq)]
 pub enum CellState {
     Empty,
     Circle,
@@ -24,21 +26,22 @@ impl Display for CellState {
     }
 }
 
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Clone)]
 pub struct TicTacToeState {
     board: Vec<Vec<CellState>>,
+    required_sequence_length: usize,
     current_player: PlayerId,
 }
 
 impl Display for TicTacToeState {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        for line in &self.board {
-            let line: String = line
+        for row in &self.board {
+            let row: String = row
                 .iter()
                 .map(ToString::to_string)
                 .intersperse(" | ".to_string())
                 .collect();
-            writeln!(f, "{line}")?;
+            writeln!(f, "{row}")?;
         }
 
         Ok(())
@@ -46,7 +49,11 @@ impl Display for TicTacToeState {
 }
 
 impl TicTacToeState {
-    pub fn new(board_size: usize) -> Self {
+    fn new(board_size: usize, required_sequence_length: usize) -> Result<Self> {
+        if required_sequence_length > board_size {
+            bail!("Impossible endgame condition: required sequence length must be less or equal the board length");
+        }
+
         let board = std::iter::repeat_with(|| {
             std::iter::repeat(CellState::Empty)
                 .take(board_size)
@@ -55,25 +62,41 @@ impl TicTacToeState {
         .take(board_size)
         .collect();
 
-        Self {
+        Ok(Self {
             board,
+            required_sequence_length,
             current_player: PlayerId::Cross,
-        }
+        })
     }
 }
 
-#[derive(Serialize, Deserialize, Debug, PartialEq, Clone, Copy)]
+impl Default for TicTacToeState {
+    fn default() -> Self {
+        Self::new(3, 3).expect("Default TicTacToe game")
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Clone, Copy)]
 pub enum PlayerId {
     Circle,
     Cross,
 }
 
+#[derive(Serialize, Deserialize, Clone, Copy, Debug)]
+pub struct Position {
+    pub row: usize,
+    pub column: usize,
+}
+
+impl Position {
+    pub fn new(row: usize, column: usize) -> Self {
+        Self { row, column }
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone, Copy, Debug)]
 pub enum Action {
-    MarkBoard {
-        player_id: PlayerId,
-        line: usize,
-        column: usize,
-    },
+    MarkBoard { player_id: PlayerId, pos: Position },
 }
 
 pub struct TicTacToeEngine {
@@ -84,32 +107,88 @@ impl TicTacToeEngine {
     pub fn new(state: TicTacToeState) -> Self {
         Self { state }
     }
+
+    fn player_result(&self, player_id: PlayerId) -> EndGameState {
+        let player_mark = match player_id {
+            PlayerId::Circle => CellState::Circle,
+            PlayerId::Cross => CellState::Cross,
+        };
+
+        let board = &self.state.board;
+        let board_size = board.len();
+        let required_sequence_length = self.state.required_sequence_length;
+
+        // Check victory condition: row
+        for rows in board {
+            for window in rows.windows(required_sequence_length) {
+                if window.iter().all(|x| *x == player_mark) {
+                    return EndGameState::Winner(player_id);
+                }
+            }
+        }
+
+        // Check victory condition: column
+        for col in 0..board_size {
+            for start_col in 0..(board_size - required_sequence_length + 1) {
+                let window: Vec<_> = board
+                    .iter_column(col)
+                    .skip(start_col)
+                    .take(required_sequence_length)
+                    .collect();
+                if window.iter().all(|x| **x == player_mark)
+                    && window.len() == required_sequence_length
+                {
+                    return EndGameState::Winner(player_id);
+                }
+            }
+        }
+
+        // TODO: victory condition: diagonal
+
+        EndGameState::GameNotOver
+    }
+
+    pub fn has_empty_cell(&self) -> bool {
+        let mut has_empty_cell = true;
+        for row in &self.state.board {
+            has_empty_cell = has_empty_cell && row.iter().any(|x| *x == CellState::Empty);
+        }
+
+        has_empty_cell
+    }
 }
 
-impl tabua_engine::Engine for TicTacToeEngine {
-    type PublicState<'a> = TicTacToeState where Self: 'a;
-    type PrivateState<'a> = ();
+#[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Clone, Copy)]
+pub enum EndGameState {
+    GameNotOver,
+    Tie,
+    Winner(PlayerId),
+}
+
+#[async_trait]
+impl tabua_engine::Engine<'_> for TicTacToeEngine {
+    type PublicState = TicTacToeState;
+    type PrivateState = ();
     type PlayerId = PlayerId;
     type Action = Action;
-    type EndGame = ();
+    type EndGame = EndGameState;
 
-    fn public_state(&self) -> &Self::PublicState<'_> {
-        &self.state
+    async fn public_state(&self) -> Result<Self::PublicState> {
+        Ok(self.state.clone())
     }
-    fn private_state(&self, _user: &Self::PlayerId) -> &'_ [&Self::PublicState<'_>] {
-        &[]
+    async fn private_state(&self, _user: &Self::PlayerId) -> Result<Vec<Self::PrivateState>> {
+        Ok(vec![])
     }
 
-    fn is_action_valid(&self, action: &Self::Action) -> Result<()> {
+    async fn validate_action(&self, action: &Self::Action) -> Result<()> {
         match action {
             Action::MarkBoard {
                 player_id,
-                line,
-                column,
+                pos: Position { row, column },
             } => {
                 let state = &self.state;
 
-                if *line >= state.board.len() || *column >= state.board[0].len() {
+                if *row >= state.board.len() || *column >= state.board[0].len() {
                     bail!("Invalid move, cell already marked");
                 }
 
@@ -117,51 +196,72 @@ impl tabua_engine::Engine for TicTacToeEngine {
                     bail!("Playing out of turn");
                 }
 
-                if state.board[*line][*column] != CellState::Empty {
+                if state.board[*row][*column] != CellState::Empty {
                     bail!("Invalid move, cell already marked");
                 }
             }
         }
 
+        if !self.has_empty_cell() {
+            bail!("No moves available")
+        }
+
+        if self.results().await? != EndGameState::GameNotOver {
+            bail!("Game over");
+        }
+
         Ok(())
     }
-    fn apply_action(&mut self, action: Action) -> Result<&Self::PublicState<'_>> {
-        self.is_action_valid(&action)?;
+    async fn apply_action(&mut self, action: Action) -> Result<()> {
+        self.validate_action(&action).await?;
 
         match action {
             Action::MarkBoard {
                 player_id,
-                line,
-                column,
+                pos: Position { row, column },
             } => {
                 let state = &mut self.state;
 
                 match player_id {
                     PlayerId::Circle => {
-                        state.board[line][column] = CellState::Circle;
+                        state.board[row][column] = CellState::Circle;
                         self.state.current_player = PlayerId::Cross;
                     }
                     PlayerId::Cross => {
-                        state.board[line][column] = CellState::Cross;
+                        state.board[row][column] = CellState::Cross;
                         self.state.current_player = PlayerId::Circle;
                     }
                 }
 
-                Ok(&self.state)
+                Ok(())
             }
         }
     }
 
-    fn current_players(&self) -> Vec<Self::PlayerId> {
-        vec![self.state.current_player]
+    async fn current_players(&self) -> Result<Vec<Self::PlayerId>> {
+        Ok(vec![self.state.current_player])
     }
 
-    fn is_over(&self) -> bool {
-        todo!()
-    }
+    async fn results(&self) -> Result<Self::EndGame> {
+        let result = match (
+            self.player_result(PlayerId::Cross),
+            self.player_result(PlayerId::Circle),
+        ) {
+            (EndGameState::GameNotOver, EndGameState::Winner(player_id))
+            | (EndGameState::Winner(player_id), EndGameState::GameNotOver) => {
+                EndGameState::Winner(player_id)
+            }
+            (EndGameState::GameNotOver, _) | (_, EndGameState::GameNotOver) => {
+                EndGameState::GameNotOver
+            }
+            _ => unreachable!(),
+        };
 
-    fn results(&self) -> Result<Self::EndGame> {
-        todo!()
+        if !matches!(result, EndGameState::Winner(_)) && !self.has_empty_cell() {
+            return Ok(EndGameState::Tie);
+        }
+
+        Ok(result)
     }
 }
 
@@ -171,57 +271,61 @@ mod tests {
 
     use super::*;
 
-    #[test]
-    fn get_initial_state() {
-        let engine = TicTacToeEngine::new(TicTacToeState::new(3));
-        let current_state = engine.public_state();
+    #[tokio::test]
+    async fn get_initial_state() {
+        let engine = TicTacToeEngine::new(TicTacToeState::default());
+        let current_state = engine.public_state().await.unwrap();
         println!("{current_state}");
 
-        assert_eq!(*current_state, TicTacToeState::new(3));
+        assert_eq!(current_state, TicTacToeState::default());
     }
 
-    #[test]
-    fn one_move() {
-        let mut engine = TicTacToeEngine::new(TicTacToeState::new(3));
-        let new_state = engine
+    #[tokio::test]
+    async fn one_move() {
+        let mut engine = TicTacToeEngine::new(TicTacToeState::default());
+        engine
             .apply_action(Action::MarkBoard {
                 player_id: PlayerId::Cross,
-                line: 0,
-                column: 0,
+                pos: Position::new(0, 0),
             })
+            .await
             .unwrap();
 
+        let new_state = engine.public_state().await.unwrap();
+
         let expected = {
-            let mut state = TicTacToeState::new(3);
+            let mut state = TicTacToeState::default();
             state.board[0][0] = CellState::Cross;
             state.current_player = PlayerId::Circle;
             state
         };
 
         println!("{new_state}");
-        assert_eq!(*new_state, expected);
+        assert_eq!(new_state, expected);
     }
 
-    #[test]
-    fn two_moves() {
-        let mut engine = TicTacToeEngine::new(TicTacToeState::new(3));
+    #[tokio::test]
+    async fn two_moves() {
+        let mut engine = TicTacToeEngine::new(TicTacToeState::default());
         engine
             .apply_action(Action::MarkBoard {
                 player_id: PlayerId::Cross,
-                line: 0,
-                column: 0,
+                pos: Position::new(0, 0),
             })
+            .await
             .unwrap();
-        let new_state = engine
+        engine
             .apply_action(Action::MarkBoard {
                 player_id: PlayerId::Circle,
-                line: 1,
-                column: 0,
+                pos: Position::new(1, 0),
             })
+            .await
             .unwrap();
 
+        let new_state = engine.public_state().await.unwrap();
+
         let expected = {
-            let mut state = TicTacToeState::new(3);
+            let mut state = TicTacToeState::default();
             state.board[0][0] = CellState::Cross;
             state.board[1][0] = CellState::Circle;
             state.current_player = PlayerId::Cross;
@@ -229,18 +333,122 @@ mod tests {
         };
 
         println!("{new_state}");
-        assert_eq!(*new_state, expected);
+        assert_eq!(new_state, expected);
     }
 
-    #[test]
-    fn wrong_player() {
-        let mut engine = TicTacToeEngine::new(TicTacToeState::new(3));
+    #[tokio::test]
+    async fn invalid_move() {
+        let mut engine = TicTacToeEngine::new(TicTacToeState::default());
+        engine
+            .apply_action(Action::MarkBoard {
+                player_id: PlayerId::Cross,
+                pos: Position::new(0, 0),
+            })
+            .await
+            .unwrap();
+
         assert!(engine
             .apply_action(Action::MarkBoard {
                 player_id: PlayerId::Circle,
-                line: 0,
-                column: 0,
+                pos: Position::new(0, 0),
             })
+            .await
+            .is_err())
+    }
+
+    #[tokio::test]
+    async fn wrong_player() {
+        let mut engine = TicTacToeEngine::new(TicTacToeState::default());
+        assert!(engine
+            .apply_action(Action::MarkBoard {
+                player_id: PlayerId::Circle,
+                pos: Position::new(0, 0),
+            })
+            .await
             .is_err());
+    }
+
+    #[tokio::test]
+    async fn game_not_over() {
+        let engine = TicTacToeEngine::new(TicTacToeState::default());
+        assert_eq!(engine.results().await.unwrap(), EndGameState::GameNotOver);
+    }
+
+    #[tokio::test]
+    async fn player_cross_victory_row() {
+        let mut engine = TicTacToeEngine::new(TicTacToeState::default());
+        assert_eq!(engine.results().await.unwrap(), EndGameState::GameNotOver);
+        engine.state.board = vec![
+            vec![CellState::Cross, CellState::Cross, CellState::Cross],
+            vec![CellState::Empty, CellState::Empty, CellState::Empty],
+            vec![CellState::Empty, CellState::Empty, CellState::Empty],
+        ];
+        assert_eq!(
+            engine.results().await.unwrap(),
+            EndGameState::Winner(PlayerId::Cross)
+        );
+    }
+
+    #[tokio::test]
+    async fn player_cross_victory_column() {
+        let mut engine = TicTacToeEngine::new(TicTacToeState::default());
+        assert_eq!(engine.results().await.unwrap(), EndGameState::GameNotOver);
+        engine.state.board = vec![
+            vec![CellState::Cross, CellState::Empty, CellState::Empty],
+            vec![CellState::Cross, CellState::Empty, CellState::Empty],
+            vec![CellState::Cross, CellState::Empty, CellState::Empty],
+        ];
+        assert_eq!(
+            engine.results().await.unwrap(),
+            EndGameState::Winner(PlayerId::Cross)
+        );
+    }
+
+    #[tokio::test]
+    async fn player_circle_victory_row() {
+        let mut engine = TicTacToeEngine::new(TicTacToeState::default());
+        assert_eq!(engine.results().await.unwrap(), EndGameState::GameNotOver);
+        engine.state.board = vec![
+            vec![CellState::Empty, CellState::Empty, CellState::Empty],
+            vec![CellState::Circle, CellState::Circle, CellState::Circle],
+            vec![CellState::Empty, CellState::Empty, CellState::Empty],
+        ];
+        assert_eq!(
+            engine.results().await.unwrap(),
+            EndGameState::Winner(PlayerId::Circle)
+        );
+    }
+
+    #[tokio::test]
+    async fn player_circle_victory_column() {
+        let mut engine = TicTacToeEngine::new(TicTacToeState::default());
+        assert_eq!(engine.results().await.unwrap(), EndGameState::GameNotOver);
+        engine.state.board = vec![
+            vec![CellState::Empty, CellState::Circle, CellState::Empty],
+            vec![CellState::Empty, CellState::Circle, CellState::Empty],
+            vec![CellState::Empty, CellState::Circle, CellState::Empty],
+        ];
+        assert_eq!(
+            engine.results().await.unwrap(),
+            EndGameState::Winner(PlayerId::Circle)
+        );
+    }
+
+    #[tokio::test]
+    async fn no_moves_available() {
+        let mut engine = TicTacToeEngine::new(TicTacToeState::default());
+        assert_eq!(engine.results().await.unwrap(), EndGameState::GameNotOver);
+        engine.state.board = vec![
+            vec![CellState::Circle, CellState::Cross, CellState::Circle],
+            vec![CellState::Cross, CellState::Cross, CellState::Circle],
+            vec![CellState::Circle, CellState::Circle, CellState::Cross],
+        ];
+        let action = Action::MarkBoard {
+            player_id: PlayerId::Circle,
+            pos: Position::new(0, 0),
+        };
+        assert!(engine.validate_action(&action).await.is_err());
+        assert!(engine.apply_action(action).await.is_err());
+        assert_eq!(engine.results().await.unwrap(), EndGameState::Tie);
     }
 }
